e59a8b0c6cfe51642997e5f0601adfd132316abd
Asami <aoi_ao726@yahoo.co.jp>
2021-12-05 20:47:43 +0900

    ハンバーガー閉じるボタン

--- js/script.js
+++ js/script.js
@@ -1,22 +1,41 @@
 $(function(){
-    // ハンバーガーメニューをクリックしたときの処理
-    $(".p-header__button").click(function () {
+    // Menuボタンをクリックしたときの処理
+    $(".p-menu-button__open").click(function () {
         $(this).toggleClass('open');
         $(".l-grid__item3").toggleClass('open');
+        $(".p-menu-button__close").toggleClass('active');
         $(".c-overlay").fadeToggle(600);
     });
-    
-    // ハンバーガーメニューのリンクをクリックしたときの処理
-    $(".p-sidebar a").click(function () {
-        $(".p-header__button").removeClass('open');
+
+    // 閉じるボタンをクリックしたときの処理
+    $(".p-menu-button__close").click(function () {
+        $(this).removeClass('active');
+        $(".p-menu-button__open").removeClass('open');
         $(".l-grid__item3").removeClass('open');
+        $(".c-overlay").fadeOut();
+    });
+    
+    // サイドバー内のリンクをクリックしたときの処理
+    $(".p-sidebar a").click(function () {
+        $(".p-menu-button__open").removeClass('open');
+        $(".l-grid__item3").removeClass('open');
+        $(".p-menu-button__close").removeClass('active');
+        $(".c-overlay").fadeOut();
+    });
+
+    // サイドバーの領域外をクリックしたときの処理
+    $(".c-overlay").click(function () {
+        $(".p-menu-button__open").removeClass('open');
+        $(".l-grid__item3").removeClass('open');
+        $(".p-menu-button__close").removeClass('active');
         $(".c-overlay").fadeOut();
     });
 
     // 画面がリサイズされたときの処理
     $(window).resize(function(){
-    $(".p-header__button").removeClass('open');
+    $(".p-menu-button__open").removeClass('open');
     $(".l-grid__item3").removeClass('open');
+    $(".p-menu-button__close").removeClass('active');
     $(".c-overlay").fadeOut();
     });
 });
